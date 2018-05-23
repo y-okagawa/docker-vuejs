884761efe34ff88bf9d1e58cf6c19efe40a36eee
y-okagawa <yuya.okagawa@gmail.com>
2018-05-23 23:46:59 +0900

    add template sample

---
+++ vuejs/src/components/vue-sample.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <label>
+            <input type="text" v-model="itemName">
+        </label>
+        <button v-on:click="addItem(itemName)">追加</button>
+        <ul>
+            <li v-for="item in items">{{ item.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "vue-sample",
+        data: function () {
+            return {
+                items: [
+                    {name: "item-AAA"},
+                    {name: "item-BBB"}
+                ],
+                itemName: ""
+            }
+        },
+        methods: {
+            addItem: function (name) {
+                this.items.push({name: name})
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
